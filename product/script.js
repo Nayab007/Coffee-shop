daa1ad105df061805f47b3f1e3e6731294b76393
Nayab Lone <55736152+Nayab007@users.noreply.github.com>
2021-10-05 02:36:33 +0200

    update

--- product/script.js
+++ product/script.js
@@ -1,93 +1,131 @@
-var textOverImages = document.getElementsByClassName("onClickTextOverImage");
-var previousTextOverImage;
- for (var i = 0; i < textOverImages.length; i++) {
-  textOverImages[i].onclick = function() {
-    var classes = this.classList;
-    if (classes.contains("show")) {
-      classes.remove("show");
-    } else {
-      if (previousTextOverImage != null)
-        previousTextOverImage.classList.remove("show");
-      previousTextOverImage = this;
-      classes.add("show");
-    }
-  }
-}
-
-function stopPropagation(event){
-  event.stopPropagation();
-}
-
-var textOverImages = document.getElementsByClassName("onClickTextOverImage1");
+/* Name Animation function */
+var textOverImages = document.getElementsByClassName('onClickTextOverImage');
 var previousTextOverImage;
 for (var i = 0; i < textOverImages.length; i++) {
-  textOverImages[i].onclick = function() {
+  textOverImages[i].onclick = function () {
     var classes = this.classList;
-    if (classes.contains("show")) {
-      classes.remove("show");
+    if (classes.contains('show')) {
+      classes.remove('show');
     } else {
       if (previousTextOverImage != null)
-        previousTextOverImage.classList.remove("show");
+        previousTextOverImage.classList.remove('show');
       previousTextOverImage = this;
-      classes.add("show");
+      classes.add('show');
     }
-  }
+  };
 }
+/* Name Animation function End */
 
-function stopPropagation(event){
+/* Animation for mushrooms and bean */
+
+function stopPropagation(event) {
   event.stopPropagation();
 }
-var textOverImages = document.getElementsByClassName("onClickTextOverImage2");
+
+var textOverImages = document.getElementsByClassName('onClickTextOverImage1');
 var previousTextOverImage;
 for (var i = 0; i < textOverImages.length; i++) {
-  textOverImages[i].onclick = function() {
+  textOverImages[i].onclick = function () {
     var classes = this.classList;
-    if (classes.contains("show")) {
-      classes.remove("show");
+    if (classes.contains('show')) {
+      classes.remove('show');
     } else {
       if (previousTextOverImage != null)
-        previousTextOverImage.classList.remove("show");
+        previousTextOverImage.classList.remove('show');
       previousTextOverImage = this;
-      classes.add("show");
+      classes.add('show');
     }
-  }
+  };
 }
 
-function stopPropagation(event){
+function stopPropagation(event) {
   event.stopPropagation();
 }
-var textOverImages = document.getElementsByClassName("onClickTextOverImage3");
+var textOverImages = document.getElementsByClassName('onClickTextOverImage2');
 var previousTextOverImage;
 for (var i = 0; i < textOverImages.length; i++) {
-  textOverImages[i].onclick = function() {
+  textOverImages[i].onclick = function () {
     var classes = this.classList;
-    if (classes.contains("show")) {
-      classes.remove("show");
+    if (classes.contains('show')) {
+      classes.remove('show');
     } else {
       if (previousTextOverImage != null)
-        previousTextOverImage.classList.remove("show");
+        previousTextOverImage.classList.remove('show');
       previousTextOverImage = this;
-      classes.add("show");
+      classes.add('show');
     }
-  }
+  };
 }
 
-function stopPropagation(event){
+function stopPropagation(event) {
   event.stopPropagation();
 }
-
-
-
-      var i=0;
-        text = document.getElementById("result").innerHTML=localStorage.getItem("textvalue");
-    function typing() {
-         if (i<text.length){
-           
-           document.getElementById("result").innerHTML += text.charAt(i);
-            i++;
-            setTimeout(typing,400);
-         }
+var textOverImages = document.getElementsByClassName('onClickTextOverImage3');
+var previousTextOverImage;
+for (var i = 0; i < textOverImages.length; i++) {
+  textOverImages[i].onclick = function () {
+    var classes = this.classList;
+    if (classes.contains('show')) {
+      classes.remove('show');
+    } else {
+      if (previousTextOverImage != null)
+        previousTextOverImage.classList.remove('show');
+      previousTextOverImage = this;
+      classes.add('show');
     }
-    typing();
-    
-    document.getElementById("demo").innerHTML = hello;
+  };
+}
+
+function stopPropagation(event) {
+  event.stopPropagation();
+}
+/* Coffee & Mushroom Animation function End */
+
+/* Result Animation function from enter first page*/
+
+var i = 0;
+text = document.getElementById('result').innerHTML =
+  localStorage.getItem('textvalue');
+function typing() {
+  if (i < text.length) {
+    document.getElementById('result').innerHTML += text.charAt(i);
+    i++;
+    setTimeout(typing, 400);
+  }
+}
+typing();
+
+document.getElementById('demo').innerHTML = hello;
+
+/* Result Animation function from enter first page end*/
+
+/* Add & subtract function */
+var incrementPlus;
+var incrementMinus;
+
+var buttonPlus = $('.cart-qty-plus');
+var buttonMinus = $('.cart-qty-minus');
+
+var incrementPlus = buttonPlus.click(function () {
+  var $n = $(this)
+    .parent('.button-container')
+    .parent('.container')
+    .find('.qty');
+  var amount = Number($n.val());
+  if (amount < 100) {
+    $n.val(amount + 5);
+  }
+});
+
+var incrementMinus = buttonMinus.click(function () {
+  var $n = $(this)
+    .parent('.button-container')
+    .parent('.container')
+    .find('.qty');
+  var amount = Number($n.val());
+  if (amount > 0) {
+    $n.val(amount - 5);
+  }
+});
+
+/* Add & subtract function end */
